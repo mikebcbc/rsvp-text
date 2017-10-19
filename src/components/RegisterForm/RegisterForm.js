2654059af47a5042af48fd7f891d785fa2ac62b0
mikebcbc <mikebconstantino@gmail.com>
2017-10-19 15:15:39 -0400

    Custom input component, form validators

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import {Field, reduxForm} from 'redux-form';
 import {registerUser} from '../../actions';
 
+import Input from '../Input/Input';
+import {required, nonEmpty, matches, isTrimmed} from '../validators.js';
+
 import './RegisterForm.css';
 
 export class RegisterForm extends Component {
   onSubmit(values) {
-  	const {name, username, password} = values;
-  	const user = {name, username, password};
+  	const {name, email, password} = values;
+  	const user = {name, email, password};
   	return this.props.dispatch(registerUser({"user": user}));
   }
 
   render() {
     return (
       <form className="register-form" onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
-      	<Field component="input" type="text" name="name" />
-      	<Field component="input" type="text" name="username" />
-      	<Field component="input" type="text" name="password" />
-      	<button type="submit">Register</button>
+      	<Field component={Input} type="text" name="name" validate={required} />
+      	<Field component={Input} type="email" name="email" validate={[required, nonEmpty, isTrimmed]} />
+      	<Field component={Input} type="text" name="password" validate={[required, isTrimmed]} />
+      	<button type="submit" disabled={this.props.pristine || this.props.submitting}>Register</button>
       </form>
     )
   }
