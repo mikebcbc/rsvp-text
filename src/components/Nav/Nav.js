83dca448a190c6d77bf3547da2914ba3e8dcc115
mikebcbc <mikebconstantino@gmail.com>
2017-10-24 15:46:49 -0400

    Redirect and clear token & info on logout

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {setCurrentUser, setAuthToken} from '../../actions';
+import {clearAuthToken} from '../../local-storage';
 import './Nav.css';
 
 class Nav extends Component {
+  logOut() {
+    this.props.dispatch(setCurrentUser(null));
+    this.props.dispatch(setAuthToken(null));
+    clearAuthToken();
+  }
+
   render() {
     return (
       <nav className="navigation">
 				<ul className="main-nav">
-          <li><Link to="/login">Login</Link></li>
-					<li><Link to="/register">Register</Link></li>
+          <li><Link to="/register">Register</Link></li>
+          {
+            (this.props.loggedIn)
+              ? <li><Link to="/" onClick={() => this.logOut()}>Logout</Link></li>
+              : <li><Link to="/login">Login</Link></li>
+          }
 				</ul>
       </nav>
     );
   }
 }
 
-export default Nav;
+const mapStateToProps = state => ({
+  loggedIn: state.rsvp.authToken !== null
+});
+
+export default connect(mapStateToProps)(Nav);
